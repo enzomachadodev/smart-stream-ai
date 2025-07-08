f1094730b5d116cc6ae79b1b5f700e0d84f936ad
Enzo Machado <enzomachadodev@gmail.com>
2025-07-08 13:38:03 -0300

    feat: create rooms listing route

---
+++ server/src/server.ts
@@ -0,0 +1,30 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+
+const port = env.PORT;
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+	origin: "http://localhost:5173",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.get("/health", () => {
+	return "OK";
+});
+
+app.register(getRoomsRoute);
+
+app.listen({ port }).then(() => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
