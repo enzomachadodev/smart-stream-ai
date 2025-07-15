e783c94ad1e45f4e53a49dda7351b2a369f854ae
Enzo Machado <enzomachadodev@gmail.com>
2025-07-15 15:51:06 -0300

    feat: add custom scrollbar styles to improve user experience

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -2,22 +2,28 @@ import { reset, seed } from "drizzle-seed";
 import { db, sql } from "./connection.ts";
 import { schema } from "./schema/index.ts";
 
+console.log("Resetting database...");
+
 await reset(db, schema);
 
-await seed(db, schema).refine((f) => {
-	return {
-		rooms: {
-			count: 20,
-			columns: {
-				name: f.companyName(),
-				description: f.loremIpsum(),
+console.log("Database reset complete. Seeding data...");
+
+await seed(db, { rooms: schema.rooms, questions: schema.questions }).refine(
+	(f) => {
+		return {
+			rooms: {
+				count: 20,
+				columns: {
+					name: f.companyName(),
+					description: f.loremIpsum(),
+				},
 			},
-		},
-		questions: {
-			count: 50,
-		},
-	};
-});
+			questions: {
+				count: 50,
+			},
+		};
+	}
+);
 
 await sql.end();
 
